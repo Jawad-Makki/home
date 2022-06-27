cfa1a662522ec7ee033a25e0fd564625f0d63c1a
Jawad-Makki <106933585+Jawad-Makki@users.noreply.github.com>
2022-06-26 19:46:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const developer = document.querySelector(".developer");
 
 const portfolioContainer = document.querySelector(".portfolio-container");
 const portfolioBg = document.querySelector(".portfolio-bg");
+const sites = document.querySelectorAll(".site");
+const contact = document.querySelector(".contact");
 
 const contactContainer = document.querySelector(".contact-container");
 
@@ -14,6 +16,11 @@ function lightMode () {
     contactContainer.style.background = "white";
     myName.style.color = "white";
     developer.style.color = "white";
+    contact.style.color = "white";
+    for (i = 0; i < sites.length; i++) {
+        sites[i].style.background = "white";
+        sites[i].style.color = "rgb(1, 12, 15)"
+    }
 }
 
 function darkMode () {
@@ -22,4 +29,9 @@ function darkMode () {
     contactContainer.style.background = "rgb(2, 35, 43)";
     myName.style.color = "rgb(1, 12, 15)";
     developer.style.color = "rgb(1, 12, 15)";
+    contact.style.color = "rgb(1, 12, 15)";
+    for (i = 0; i < sites.length; i++) {
+        sites[i].style.background = "rgb(2, 35, 43)";
+        sites[i].style.color = "white";
+    }
 }
