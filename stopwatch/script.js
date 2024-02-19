2a9e7933145fa4476b6944d0495c1efb3b698ae8
Jawad-Makki <106933585+Jawad-Makki@users.noreply.github.com>
2024-02-18 23:39:27 -0800

    Add files via upload

---
+++ stopwatch/script.js
@@ -0,0 +1,56 @@
+const recordButton = document.getElementById("record-button");
+const time = document.getElementById("time");
+
+var mic;
+var recording = false;
+var startTime;
+var endTime;
+
+function setup () {
+    if (recording) {
+        mic = new p5.AudioIn();
+        mic.start();
+    }
+}
+
+function draw () {
+    if (recording) {
+        var volume = mic.getLevel();
+        console.log(volume);
+
+        if (volume > 0.2) {
+            startOrStop();
+        }
+
+        if (startTime != null && endTime == null) {
+            now = new Date().getTime();
+            time.innerHTML = (now - startTime) / 1000;
+        }
+    }
+}
+
+function toggleRecording () {
+    if (recording) {
+        recordButton.innerHTML = "Start Recording";
+        mic.stop();
+    } else {
+        recordButton.innerHTML = "Stop Recording";
+    }
+
+    recording = !recording;
+}
+
+function startOrStop () {
+    if (startTime == null) {
+        startTime = new Date().getTime();
+    } else {
+        endTime = new Date().getTime();
+        time.innerHTML = (endTime - startTime) / 1000;
+    }
+}
+
+function reset () {
+    startTime = null;
+    endTime = null;
+    recording = false;
+}
